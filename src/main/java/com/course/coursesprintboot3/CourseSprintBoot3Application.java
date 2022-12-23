e78b4ed5fd5f36beb5686ff9380adef12b972682
Iury <iurygfranca@gmail.com>
2022-12-23 17:25:15 -0400

    inital settings

---
+++ src/main/java/com/course/coursesprintboot3/CourseSprintBoot3Application.java
@@ -0,0 +1,13 @@
+package com.course.coursesprintboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSprintBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSprintBoot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/coursesprintboot3/CourseSprintBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.coursesprintboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSprintBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
