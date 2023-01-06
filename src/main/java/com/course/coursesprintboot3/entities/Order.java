74db997ed961cdd5f4d2c99876d7223e36c5007e
Iury <iurygfranca@gmail.com>
2023-01-06 15:30:14 -0400

    feat: Subtotal & Total methods ✨

--- src/main/java/com/course/coursesprintboot3/entities/Order.java
+++ src/main/java/com/course/coursesprintboot3/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         }
     }
 
+    public Double getTotal(){
+        Double total = 0.0;
+        for (OrderItem item : items) {
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
